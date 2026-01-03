347501532332fe2437451609014207aeeccbf3fc
Kim Hyeong Seop <fbhydfa@gmail.com>
2026-01-03 16:15:36 +0900

    first commit

---
+++ src/main/java/com/spring/prcjsp/PrcJspApplication.java
@@ -0,0 +1,13 @@
+package com.spring.prcjsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrcJspApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrcJspApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/prcjsp/PrcJspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.prcjsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrcJspApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
